821e80924e171f4ac2ac796fb5a8d384e343d599
IT24102684 <it24102684@my.sliit.lk>
2024-10-03 20:48:33 +0530

    Lab7Q2A, Lab7Q2B &Lab7Q2C

---
+++ Q2/IT24102684Lab7Q2B.java
@@ -0,0 +1,13 @@
+public class IT24102684Lab7Q2B {
+    public static void main(String[] args) {
+        int i , j ;
+        for (i = 1; i <= 5; i++) {
+            System.out.print(i + " - " );
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print ("\n");
+        }
+
+    }
+}
